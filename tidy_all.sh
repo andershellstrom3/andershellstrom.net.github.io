0f1560ed1af3abb45d45d3b2a2abd9a49aad28f5
qBV4Q <151439257+qBV4Q@users.noreply.github.com>
2023-11-28 15:19:32 +0100

    Add files via upload

--- tidy_all.sh
+++ tidy_all.sh
@@ -1,8 +1,8 @@
-tidy -utf8 -i -m -c -omit -q -asxml index.html 
-tidy -utf8 -i -m -c -omit -q -asxml index.html
-tidy -utf8 -i -m -c -omit -q -asxml mermaid.html 
+tidy -utf8 -i -m -c -omit -q -asxml index.xhtml 
+tidy -utf8 -i -m -c -omit -q -asxml index.xhtml
+tidy -utf8 -i -m -c -omit -q -asxml mermaid.xhtml 
 tidy -utf8 -i -m -c -omit -q -asxml mermaid.html 
 clear
 ###Access check
-tidy -utf8 -i -m -c -access 3 -omit -asxml index.html
-tidy -utf8 -i -m -c -access 3 -omit -asxml mermaid.html 
+tidy -utf8 -i -m -c -access 3 -omit -asxml index.xhtml
+tidy -utf8 -i -m -c -access 3 -omit -asxml mermaid.xhtml 
